33b2de272fd8c14203578910c8a251e166dd7bee
Cristian1231233 <campagnacristian22@gmail.com>
2021-10-27 17:37:33 +0200

    altro commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,27 @@
 // Creare carosello
 // al click l'immagine grande diventa visibile e l'immagine piccola a destra si evidenzia rispetto alle altre
 
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+const imageBig = document.querySelector('.image-big');
+let incremento = 0;
+for(let i = 0; i < items.length; i++){
+    console.log(items[i]);
+    const el = document.createElement('div');
+    el.className = 'items';
+    
+    if(i === incremento){
+        items.classList.add('active')
+    }
+
+    items.innerHTML = `<img src="${images[i]}" alt="">`;
+}
+//
 const items = document.getElementsByClassName('items');
 const item2 = document.getElementsByClassName('item2');
 
@@ -12,15 +33,15 @@ console.log(btnSu);
 const btnGiu = document.querySelector('.freccia-giu');
 console.log(btnGiu);
 
-let incremento = 0;
+
 
 console.log('incremento',incremento);
 console.log('lista',items);
 console.log('incremento lista', items[incremento]);
 
-for(let i = 0; i < items.length; i++){
-    console.log(items[i]);
-}
+// for(let i = 0; i < items.length; i++){
+//     console.log(items[i]);
+// }
 
 btnSu.addEventListener('click',function(){
     

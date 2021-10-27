621f45b7249beffe65500351ece0c2707b19e670
Cristian1231233 <campagnacristian22@gmail.com>
2021-10-27 14:15:41 +0200

    struttura commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+// Creare carosello
+// al click l'immagine grande diventa visibile e l'immagine piccola a destra si evidenzia rispetto alle altre
+
+const btnSu = document.querySelector('.freccia-su');
+console.log(btnSu);
+const btnGiu = document.querySelector('.freccia-giu');
+console.log(btnGiu);
+
+
